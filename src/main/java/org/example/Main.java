90a9e3e1de48d5b27f7bf9f19afaa464bed04bee
Palola91 <120025214+Palola91@users.noreply.github.com>
2023-09-07 21:47:43 +0200

    utmaning1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Temperatur i Celsius
+        double celsius = 25.0;
+
+        // Omvandling till Fahrenheit
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        // Omvandling till Kelvin
+        double kelvin = celsius + 273.15;
+
+        // Resultat
+        System.out.println("Temperatur i Celsius: " + celsius);
+        System.out.println("Temperatur i Fahrenheit: " + fahrenheit);
+        System.out.println("Temperatur i Kelvin: " + kelvin);
+
     }
 }
